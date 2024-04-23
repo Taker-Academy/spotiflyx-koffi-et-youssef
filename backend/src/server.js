85775337dea85b2974770151a8e649027fb4000f
ymehili <youssefmehili@gmail.com>
2024-04-23 08:38:58 +0200

    [UPDATE] Add CORS dependency to backend package.json and enable CORS in server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const PORT = process.env.PORT || 8080;
+
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
+    console.log('Hello');
 });
 
 app.listen(PORT, () => {
