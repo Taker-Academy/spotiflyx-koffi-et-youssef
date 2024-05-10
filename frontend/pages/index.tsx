2e340e7d985335f0649eac2433bc274a43d58309
ymehili <youssefmehili@gmail.com>
2024-05-10 12:02:56 +0200

    [UPDATE] migrated frontend to next.js

---
+++ frontend/pages/index.tsx
@@ -0,0 +1,12 @@
+// pages/index.tsx
+import React from 'react';
+import App from '../components/app';
+
+export default function Home() {
+    return (
+        <div>
+            {}
+            <App />
+        </div>
+    );
+}
